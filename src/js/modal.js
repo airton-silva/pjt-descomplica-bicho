c984d07766fc26efb6915e1e90d2dd22d7dd6cc9
raimundo-oliveira <71029465+raimundo-oliveira@users.noreply.github.com>
2021-07-04 20:53:18 -0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,3 +7,17 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+function Login() {
+    var done=0;
+    var usuario = document.getElementsByName('email')[0].value;
+    usuario=usuario.toLowerCase();
+    var senha= document.getElementsByName('senha')[0].value;
+    senha=senha.toLowerCase();
+    if (usuario=="admin" && senha=="admin") {
+      window.location="../home/home.html";
+      done=1;
+    }
+    if (done==0) { alert("Dados incorretos, tente novamente"); }
+  }
